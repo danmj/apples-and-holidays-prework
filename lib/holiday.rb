2441808909909e2cd16865ec64a1077b0417797e
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 14:51:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  list_of_supplies = holiday_supplies[:winter].collect do |holiday, supplies|
+      supplies
+   end
+   list_of_supplies.flatten
 
 end
 
